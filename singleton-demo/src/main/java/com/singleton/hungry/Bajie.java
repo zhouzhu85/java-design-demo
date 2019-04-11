ef5247b1d2784d836d451a926b982dccf4f4fc0d
zhouzhu85 <734837014@qq.com>
2019-04-11 15:58:53 +0800

    #单例模式# 补漏图片

--- singleton-demo/src/main/java/com/singleton/hungry/Bajie.java
+++ singleton-demo/src/main/java/com/singleton/hungry/Bajie.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Bajie extends JPanel {
     private static final Bajie instance=new Bajie();
     private Bajie(){
-        JLabel jLabel=new JLabel(new ImageIcon("E:\\myproject\\java-design-demo\\singleton-demo\\src\\5243c949aadb6.jpg"));
+        JLabel jLabel=new JLabel(new ImageIcon("singleton-demo/src/Bajie.jpg"));
         this.add(jLabel);
     }
     public static Bajie getInstance(){
